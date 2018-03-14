4316536d8337cdc8ebcfe197ea924e78eee55f9f
cd healthyTOD <sanath.bharadwaj21@gmail.com>
2018-03-14 16:46:10 +0530

    Changed pins

--- motor.py
+++ motor.py
@@ -1,13 +1,16 @@
 import time
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(10, 11, 13, 15, GPIO.OUT)
+GPIO.setup(31, GPIO.OUT)
+GPIO.setup(33, GPIO.OUT)
+GPIO.setup(35, GPIO.OUT)
+GPIO.setup(37, GPIO.OUT)
 
 while true:
     a = input("Enter 1 or 2")
     if a is 1 :
-        GPIO.output(10, True)
-        GPIO.output(11, False)
+        GPIO.output(31, True)
+        GPIO.output(33, False)
     else:
-        GPIO.output(13, True)
-        GPIO.output(15, False)
+        GPIO.output(35, True)
+        GPIO.output(37, False)
